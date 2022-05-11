da30001a2e7c3334cb25cfcae9a58a87bbe9fb3a
Kovalskiii <49062638+Kovalskiii@users.noreply.github.com>
2022-05-12 00:43:16 +0300

    worker list update

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -4,6 +4,7 @@ import {
   queryWorkerSetRegisterMode
 } from "./queries";
 import pkg from 'lodash';
+import { getWorkersList } from "./workerGetAllList";
 const { get } = pkg;
 
 document.querySelector('#addWorkerBtn').addEventListener('click', () => workerAdd());
@@ -19,7 +20,70 @@ const workerAdd = async () => {
     });
 }
 
-document.querySelector('#updateWorkerBtn').addEventListener('click', () => workerUpdate());
+document.querySelector('input[type="search"]').addEventListener('input', (event) => workerSearch(event));
+
+const workerSearch = async (event) => {
+  const name = document.querySelector('#searchInput').value;
+
+  const payload = {
+    data: {
+      name: `${name}`,
+    }
+  }
+
+  if (event.target.value.length) {
+    await queryWorkerSearchByName(payload)
+      .then((data) => {
+        console.log(data.payload)
+        const workerInfoArr = data.payload;
+        const workersListContainer = document.querySelector('.workers-list-container');
+        //
+        if (workerInfoArr) {
+          workersListContainer.innerHTML = '';
+          workerInfoArr.forEach(el => {
+            workersListContainer.innerHTML += `
+
+        <div class="workers-list-item">
+            <div class="workers-list">
+                <div class="worker-photo-container">
+                    <img src="${el.photo}"
+                         alt="Italian Trulli" width="120" height="150">
+                </div>
+
+                <div class="worker-info-container">
+                    <h4>Name: ${el.name}</h4>
+                    <h4>RFID: ${el.rfid}</h4>
+                    <h4>fingerprintId: ${el.fingerprintId}</h4>
+                    <h4>workerId: ${el.id}</h4>
+                </div> 
+
+                <div class="worker-btn-container">
+                    <button class="button" id="updateWorkerBtn">Update</button>
+                    <button class="button" href="#" id="deleteWorkerBtn">Delete</button>
+                </div>
+            </div>
+            <hr>
+        </div>`
+          })
+        }
+        else {
+          console.log(data.response.data.message)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  else {
+    getWorkersList();
+  }
+}
+
+const updateBtn = document.querySelector('#updateWorkerBtn');
+
+if (updateBtn) {
+  deleteBtn.addEventListener('click', () => workerUpdate());
+}
 
 const workerUpdate = async () => {
   //
@@ -33,27 +97,11 @@ const workerUpdate = async () => {
     });
 }
 
-document.querySelector('#searchWorker').addEventListener('click', () => workerSearch());
+const deleteBtn = document.querySelector('#deleteWorkerBtn');
 
-const workerSearch = async () => {
-  const name = document.querySelector('#name').value;
-
-  const payload = {
-    data: {
-      name: `${name}`,
-    }
-  }
-
-  await queryWorkerSearchByName(payload)
-    .then((data) => {
-      console.log(data.message);//anume dupa nume live search
-    })
-    .catch((error) => {
-      console.log(error.response.data.message);
-    });
+if (deleteBtn) {
+  deleteBtn.addEventListener('click', () => workerDelete());
 }
-
-document.querySelector('#deleteWorkerBtn').addEventListener('click', () => workerDelete());
 
 const workerDelete = async () => {
   const workerId = 0; //must be get from workers list
--- src/worker/workerGetAllList.js
+++ src/worker/workerGetAllList.js
@@ -1,12 +1,46 @@
 import { queryWorkerGetAllList } from "./queries.js";
 
 
-document.querySelector('#collectDataBtn').addEventListener('click', () => getWorkersList());
+// document.querySelector('.workers-list-container').addEventListener('load', () => getWorkersList());
+document.addEventListener("DOMContentLoaded", () => getWorkersList());
 
-const getWorkersList = () => {
+export const getWorkersList = () => {
   queryWorkerGetAllList()
         .then((data) => {
-            console.log(data.payload);//din data in html ca tabel
+          const workerInfoArr = data.payload;
+          const workersListContainer = document.querySelector('.workers-list-container');
+          //
+          if (workerInfoArr) {
+            workersListContainer.innerHTML = '';
+            workerInfoArr.forEach(el => {
+              workersListContainer.innerHTML += `
+
+        <div class="workers-list-item">
+            <div class="workers-list">
+                <div class="worker-photo-container">
+                    <img src="${el.photo}"
+                         alt="Italian Trulli" width="120" height="150">
+                </div>
+
+                <div class="worker-info-container">
+                    <h4>Name: ${el.name}</h4>
+                    <h4>RFID: ${el.rfid}</h4>
+                    <h4>fingerprintId: ${el.fingerprintId}</h4>
+                    <h4>workerId: ${el.id}</h4>
+                </div> 
+
+                <div class="worker-btn-container">
+                    <button class="button" id="updateWorkerBtn">Update</button>
+                    <button class="button" href="#" id="deleteWorkerBtn">Delete</button>
+                </div>
+            </div>
+            <hr>
+        </div>`
+            })
+          }
+          else {
+            console.log(data.response.data.message)
+          }
         })
         .catch((error) => {
             console.log(error);
