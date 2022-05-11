fac31917ed2a37efe0cb9b6954a6891dd1fcbcbb
k1avel <rauamail20@gmail.com>
2022-05-11 19:30:01 +0300

    fix

--- src/worker/workerGetAllList.js
+++ src/worker/workerGetAllList.js
@@ -1,8 +1,9 @@
 import {queryWorkerGetAllList, queryWorkerGetFingerprintData} from "./queries.js";
 
-document.querySelector('#getFingerprint').addEventListener('click', () => getFingerprintId());
 
-const getFingerprintId = () => {
+document.querySelector('#getFingerprint').addEventListener('click', () => getworkerGEtAllList());
+
+const getworkerGEtAllList = () => {
   queryWorkerGetAllList()
         .then((data) => {
             console.log(data);//din data in html ca tabel
